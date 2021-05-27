494e1181d15534e436579758a97267345fa1c5ec
Agam <agam.punchhi21@gmail.com>
2021-05-27 16:52:11 +0530

    check

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const PubSub = require('./app/pubsub');
 const TransactionPool = require('./wallet/transaction-pool');
 const Wallet = require('./wallet');
 const TransactionMiner = require('./app/transaction-miner');
-const e = require('express');
 
 const isDevelopment = process.env.ENV === 'development';
 
